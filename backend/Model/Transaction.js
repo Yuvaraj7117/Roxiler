db04337580e5358175fe5ec612ce09a700b13803
YUVARAJ P <143722627+Yuvaraj7117@users.noreply.github.com>
2024-06-17 16:55:18 +0530

    Update Transaction.js

--- backend/Model/Transaction.js
+++ backend/Model/Transaction.js
@@ -1,1 +1,18 @@
 
+const mongoose=require('mongoose');
+
+const TSchema=mongoose.Schema({
+
+    id:{type:Number,required:true,unique:true},
+    title:{type:String,required:true},
+    price:{type:Number,required:true},
+    description:{type:String,required:true},
+    category:{type:String,required:true},
+    image:{type:String,required:true},
+    sold:{type:Boolean,required:true},
+    dateOfSale:{type:Date,required:true}
+
+});
+
+const Transaction=mongoose.model("roxCollections",TSchema);
+module.exports=Transaction;
